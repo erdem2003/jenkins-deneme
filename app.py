239c4208804fe7a65d02add79584cb8f2743997a
erdem2003 <erdemk2330@gmail.com>
2025-11-24 17:58:43 +0300

    added new bye endpoint

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return jsonify({"message": "Hello from Jenkins deployed API!"})
 
+@app.route("/bye")
+def bye():
+    return jsonify({"message": "Goodbye from updated API!"})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
