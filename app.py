d982be18ef67a1c7ec8ff4e25816fd6dce89c5e1
erdem2003 <erdemk2330@gmail.com>
2025-11-24 18:45:19 +0300

    add new ok

--- app.py
+++ app.py
@@ -15,5 +15,10 @@ def ping():
     return jsonify({"message": "Pong from Jenkins CI/CD!"})
 
 
+@app.route("/ok")
+def ok():
+    return jsonify({"message": "Auto deploy is REALLY working!"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
